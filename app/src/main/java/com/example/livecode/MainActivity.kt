844e662661631941dd3b585b91d5a5bf601a1064
Josh-Muleshi <jmuleshi2@gmail.com>
2022-07-30 16:18:29 +0200

    finish

--- app/src/main/java/com/example/livecode/MainActivity.kt
+++ app/src/main/java/com/example/livecode/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.livecode
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.livecode.ui.theme.LiveCodeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +30,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LiveCodeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val photos = listOf(
+                        R.drawable.ic_image1,
+                        R.drawable.ic_image2,
+                        R.drawable.ic_image3,
+                        R.drawable.ic_image4,
+                        R.drawable.ic_image5,
+                        R.drawable.ic_image6,
+                        R.drawable.ic_image8
+                    )
+                    ListPhoto(photos)
                 }
             }
         }
@@ -30,14 +51,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ListPhoto(photos : List<Int>) {
+    LazyRow(
+        contentPadding = PaddingValues(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.fillMaxSize(),
+        content = {
+            items(photos){ photo ->
+                ItemUI(photo = photo)
+            }
+        }
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    LiveCodeTheme {
-        Greeting("Android")
+fun ItemUI(photo : Int) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Card(modifier = Modifier
+            .padding(top = 24.dp, bottom = 24.dp, end = 7.dp, start = 8.dp)
+            .width(300.dp),
+            elevation = 2.dp,
+            shape = RoundedCornerShape(corner = CornerSize(10.dp))
+        ) {
+            Image(
+                painter = painterResource(id = photo),
+                contentDescription = "photo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
     }
 }
